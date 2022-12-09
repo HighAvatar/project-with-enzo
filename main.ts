0f2d7d5d5d0e35ca4da221f4abc39f8f3df57514
That One Dude <118217791+HampterSucksAtCoding@users.noreply.github.com>
2022-12-09 12:53:41 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,19 @@
-
+let Blob = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . a a a a a . . . . . 
+    . . . . a a a a a a a a a . . . 
+    . . . a a a f a a a f a a . . . 
+    . . a a a a f a a a f a a a . . 
+    . a a a a a a a a a a a a a . . 
+    a a a a a a a a a a a a a a a . 
+    `, SpriteKind.Player)
+controller.moveSprite(Blob)
